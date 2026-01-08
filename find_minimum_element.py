7850e5960a4978f73326aa783b88c1c2319234d1
shashikant kumar <sp8326441@gmail.com>
2026-01-08 21:41:32 +0530

    Update find_minimum_element.py

--- find_minimum_element.py
+++ find_minimum_element.py
@@ -6,8 +6,11 @@
 # We assume the first element as the minimum value and compare it
 # with the remaining elements one by one. If a smaller value is found,
 # we update the minimum variable.
-
-arr = [4,3,2,8,1,6]
+n = int(input("enter number:")
+arr = []
+for i in range(n):
+  arr.append(int(input()))
+  
 min = arr[0]
 
 for i in range(len(arr)):
